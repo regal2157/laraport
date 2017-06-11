876f12dc49fa434e8c8ea332546f49260576c5d5
John O'Connell <regal2157@yahoo.com>
2017-06-11 19:20:25 -0400

    passport install

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,4 +14,9 @@
         </div>
     </div>
 </div>
+
+<passport-clients></passport-clients>
+<passport-authorized-clients></passport-authorized-clients>
+<passport-personal-access-tokens></passport-personal-access-tokens>
+
 @endsection
